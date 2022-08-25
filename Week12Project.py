0f0d83541662c1132f20f77785fe36f4eb488eae
wintfam <wintfam06@gmail.com>
2022-08-25 21:27:40 +0000

    MyAWSlist

--- Week12Project.py
+++ Week12Project.py
@@ -3,4 +3,22 @@ print ("Week12, Project!!!")
 
 
 #The list should be empty initially
-list ()
+MyAWSlist = []
+
+
+#Populate the list using append or insert
+MyAWSlist.append("EC2")
+MyAWSlist.append("Lambda")
+MyAWSlist.append("Cloud9")
+MyAWSlist.append("S3")
+MyAWSlist.append("DynamoDB")
+
+
+#Print the list and the length of the list
+
+
+
+#Remove two specific services from the list by name or by index
+
+
+#Print the new list and the new length of the list
