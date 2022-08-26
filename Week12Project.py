a22dbf937f8137620c6909ee88159a8f957e880d
wintfam <wintfam06@gmail.com>
2022-08-26 20:54:14 +0000

    new Edits

--- Week12Project.py
+++ Week12Project.py
@@ -4,27 +4,27 @@ print ("Week12, Project!!!")
 
 #The list should be empty initially
 MyAWSlist = []
-print ("n\Emptylist")
-print ("MyAWSlist")
+print("n\Emptylist")
+print(MyAWSlist)
 
 #Populate the list using append or insert
+print("\nThis adds AWS services to our list:")
 MyAWSlist.append("EC2")
 MyAWSlist.append("Lambda")
 MyAWSlist.append("Cloud9")
 MyAWSlist.append("S3")
 MyAWSlist.append("DynamoDB")
-print ('MyAWSlist')
+print(MyAWSlist)
 
 
 #Print the list and the length of the list
 print ("\nThis prints out the length of our list:")
-print ("MyAWSlist")
-print (len("MyAWSlist"))
+
+print(len(MyAWSlist))
 
 #Remove two specific services from the list
-
-MyAWSlist.remove("EC2")
+MyAWSlist.remove("Lambda")
 MyAWSlist.remove("S3")
-print ("MyAWSlist")
-
+print(MyAWSlist)
 #Print the new list and the new length of the list
+print("\nThis prints out the length of our new list:")
