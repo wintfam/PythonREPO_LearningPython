47b85697c9ca2885a8d6f570a98c22733cf2b670
wintfam <wintfam06@gmail.com>
2022-09-06 18:44:55 +0000

    hoping this reads

---
+++ Week13_namegenerator/week13_nameGenerator.py
@@ -0,0 +1,11 @@
+#week 13 Project
+
+#1. All the user to input how many EC2 instances they want names for and output the same amount of unique names.
+#2. Allow the user to input the name of their department that is used in the unique name.
+#3. Generate random characters and numbers that will be included in the unique name.
+
+print("Anthony Wint")
+print(__name__)
+
+import random
+import string
