60eb3e6ec19a6225dd3496bd6530e1204983305a
wintfam <wintfam06@gmail.com>
2022-09-06 21:46:23 +0000

    change 4

--- Week13_namegenerator/week13_nameGenerator.py
+++ Week13_namegenerator/week13_nameGenerator.py
@@ -25,7 +25,7 @@ num_of_instances = input('How many EC2 instances do you want to provision?\n')
 
 department = input('What is your department?\n')
 
-print(f"You have asked to provision {num_of_instances} EC2 instances for the {department} department\n The names are: \n")
+print(f"You have asked to provision {num_of_instances} EC2 instances for the {dept} department\n The names are: \n")
 
-for _ in range (int(num_of_instances)):
-    print (str(dept),''.join([choice(string.ascii_uppercase + string.digits) for _ in range(10)]))
+for _ in range (int(ec2_count)):
+print (str(dept), ''.join([choice(string.ascii_uppercase + string.digits) for _ in range(N)]))
