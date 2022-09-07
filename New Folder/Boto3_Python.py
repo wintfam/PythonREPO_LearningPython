0c0b41b6d62173f9d7ce1417af88466ec68f2f64
wintfam <wintfam06@gmail.com>
2022-09-07 22:12:53 +0000

    s3 Bucket Created

--- New Folder/Boto3_Python.py
+++ New Folder/Boto3_Python.py
@@ -2,10 +2,11 @@
 
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.bucket("luits3_tech")
-response = bucket.create(
-    ACL='public-read'
-    )
- 
+bucket=aws_resource.Bucket("luit-s3tech-tony")
 
+response = bucket.create(
+    ACL='public-read',
+    )
+    
+    
 print(response)
