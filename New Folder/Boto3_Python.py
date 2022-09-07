33ad29ba8634cff1f0e369c611fb205c4e62e31e
wintfam <wintfam06@gmail.com>
2022-09-07 18:56:33 +0000

    Creating an "s3" bucket using boto3_python

--- New Folder/Boto3_Python.py
+++ New Folder/Boto3_Python.py
@@ -2,5 +2,15 @@
 
 import boto3
 aws_resource=boto3.resorce("s3")
-bucket=aws_resource.bucket("totaltechnology")
+bucket=aws_resource.Bucket("totaltechnology")
+response = bucket.create(
+    ACL='public-read',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'us-east-1'
+    },
+    
+)    
+ 
 
+print(response
+)
