e87dadc9c02862e5c0efcfe9a2c2b7a2f8a3316d
wintfam <wintfam06@gmail.com>
2022-09-08 18:12:04 +0000

    Private Bucket created

--- Boto3_Python_s3/Boto3_Python.py
+++ Boto3_Python_s3/Boto3_Python.py
@@ -2,10 +2,10 @@
 
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("luit-s3tech-tony")
+bucket=aws_resource.Bucket("luit-s3tech-tony2")
 
 response = bucket.create(
-    ACL='public-read',
+    ACL='private',
     )
     
     
