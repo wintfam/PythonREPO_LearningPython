f6b56f0ab18d1907247e64d0b820d8d9668bb12c
wintfam <wintfam06@gmail.com>
2022-09-08 18:55:58 +0000

    Images in buckets

--- Boto3_Python_s3/Boto3_Python.py
+++ Boto3_Python_s3/Boto3_Python.py
@@ -4,15 +4,13 @@ import boto3
 
 s3 = boto3.client('s3')
 
-#%%
+bucket_name="luit-s3tech-tony"
 
-response = s3.list_buckets()
+response = s3.list_objects(
+    Bucket=bucket_name
+)
 
-#%%
+contents = response['Contents']
 
-buckets = response["Buckets"]
-
-#%%
-
-for bucket in buckets:
-    print(bucket["Name"])
+for content in contents:
+    print(content['Key'])
